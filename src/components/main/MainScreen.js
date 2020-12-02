4f0bcf5fd0e82747bfee40d56e13b6c8d2b91767
Daniel <DanielCarrete_Dev@outlook.com>
2020-12-02 11:44:00 -0600

    list works

--- src/components/main/MainScreen.js
+++ src/components/main/MainScreen.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react'
-import {View,Text, Pressable} from 'react-native'
-//import Http from 'Rick_and_Morty_Api/src/libs/http'
+import {View,Text, Pressable, FlatList} from 'react-native'
+import Http from 'Rick_and_Morty_Api/src/libs/http'
 
 export class MainScreen extends Component {
   
-    /*
+state = {
+    characters:[]
+}
+
 componentDidMount = async () => {
-   //const personages = await Http.instance.get("https://rickandmortyapi.com/api/character/?page=34")
-   //console.log(personages)
-}*/
+    const lol = 9
+    console.log(lol)
+    
+    const res = await Http.instance.get("https://rickandmortyapi.com/api/character/?page=19")
+    this.setState({characters : res.results})
+ 
+}
 
 handlePress = () =>{
  this.props.navigation.navigate('Personage Detail')
@@ -21,6 +28,14 @@ handlePress = () =>{
                     works lol
                 </Text>
                 <Pressable onPress={this.handlePress}><Text>lolol</Text></Pressable>
+                <FlatList
+                    data={this.state.characters}
+                    keyExtractor={item => item.name.toString()}
+                    renderItem={({ item }) =>
+                    <Text>{item.name}</Text>
+                    }
+                    
+                />
             </View>
         )
     }
